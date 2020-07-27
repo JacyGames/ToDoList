6c16a4ddd1b189c5e312c90af41013f2a3a1af18
JacyGames <bohdan.danultsiv@gmail.com>
2020-07-27 21:16:07 +0300

    Updates

---
+++ precache-manifest.d82f300720d1cb9fb7735ad81a3ba27d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebd71dc67f2a939bf3aad66c572b386f",
+    "url": "/JacyGames/ToDoList/index.html"
+  },
+  {
+    "revision": "e84747b8589c88cf9d3e",
+    "url": "/JacyGames/ToDoList/static/css/main.7edadea8.chunk.css"
+  },
+  {
+    "revision": "03a84c11beb2c4f9efa8",
+    "url": "/JacyGames/ToDoList/static/js/2.72b4d8a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JacyGames/ToDoList/static/js/2.72b4d8a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e84747b8589c88cf9d3e",
+    "url": "/JacyGames/ToDoList/static/js/main.0f095e92.chunk.js"
+  },
+  {
+    "revision": "efcb758f84bf6b94fad8",
+    "url": "/JacyGames/ToDoList/static/js/runtime-main.ef2096b8.js"
+  }
+]);
